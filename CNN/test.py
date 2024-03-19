60bc212aba7274761793d21fdd86467f9ef581fe
jhn6666 <12210308@mail.sustech.edu.cn>
2024-03-20 02:08:13 +0800

    Commit messages

---
+++ CNN/test.py
@@ -0,0 +1,66 @@
+import matplotlib.pyplot as plt
+import os
+import json
+import torch.nn as nn
+from network.AlexNet import AlexNet
+
+import torch
+from PIL import Image
+from torchvision import transforms
+
+
+def main():
+    # device = torch.device("cpu")
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+    data_transform = transforms.Compose(
+        [transforms.Resize((224, 224)),
+         transforms.ToTensor(),
+         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+    # load image
+    img_path = "data/train/tulips/112428665_d8f3632f36_n.jpg"
+    assert os.path.exists(img_path), "file: '{}' dose not exist.".format(img_path)
+    img = Image.open(img_path)
+
+    # plt.switch_backend('agg')
+    plt.imshow(img)
+    # [N, C, H, W]
+    img = data_transform(img)
+    # expand batch dimension
+    img = torch.unsqueeze(img, dim=0)
+
+    # read class_indict
+    json_path = "data/class_indices.json"
+    assert os.path.exists(json_path), "file: '{}' dose not exist.".format(json_path)
+
+    json_file = open(json_path, "r")
+    class_indict = json.load(json_file)
+
+    # create model
+    model = AlexNet(num_classes=5).to(device)
+
+    # load model weights
+    weights_path = "checkpoints/alex/alex_flower.pth"
+    assert os.path.exists(weights_path), "file: '{}' dose not exist.".format(weights_path)
+    model.load_state_dict(torch.load(weights_path))
+
+    model.eval()
+    with torch.no_grad():
+        # predict class
+        output = torch.squeeze(model(img.to(device))).cpu()
+        predict = torch.softmax(output, dim=0)
+        predict_cla = torch.argmax(predict).numpy()
+
+    print_res = "class: {}   prob: {:.3}".format(class_indict[str(predict_cla)],
+                                                 predict[predict_cla].numpy())
+    plt.title(print_res)
+    for i in range(len(predict)):
+        print("class: {:10}   prob: {:.3}".format(class_indict[str(i)],
+                                                  predict[i].numpy()))
+    plt.show()
+    # plt.savefig("./AelxNet_infor.jpg")
+
+
+if __name__ == '__main__':
+    main()
--- CNN/train.py
+++ CNN/train.py
@@ -1,56 +1,109 @@
-from torchvision import transforms, datasets, models
 import torch
 import torch.nn as nn
-import torch.optim as optim
+import os
+from torchvision import transforms, datasets
 from torch.utils.data import DataLoader
 from network.AlexNet import AlexNet
+#from network.ZFNet import ZFNet
 import time
-
+import torch.optim as optim
 
 
 def main():
-    # 1. 加载数据集
+    batch_size = 128  # 每次进入模型的数据
+    num_classes = 5
+    net = AlexNet(num_classes=num_classes)
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+    # device = torch.device('cpu')
+    print('using {} device.'.format(device))
+    data_path = os.path.abspath(os.path.join("data"))
+    net.to(device)
+    loss_function = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(net.parameters(), lr=0.0002)
+
     data_transform = {
-        "train": transforms.Compose([transforms.RandomResizedCrop(224),  # 随机裁剪
-                                     transforms.RandomHorizontalFlip(),  # 随机水平翻转
-                                     transforms.ToTensor(),  # 转换为张量
-                                     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]),  # 标准化
-        "val": transforms.Compose([transforms.Resize((224, 224)),  # 重置图像分辨率
-                                   transforms.ToTensor(),
-                                   transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]),
-        "test": transforms.Compose([transforms.Resize((224, 224)),  # 重置图像分辨率
-                                   transforms.ToTensor(),
-                                   transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+        "train": transforms.Compose(
+            [transforms.RandomResizedCrop(224),  # 随机裁剪
+             transforms.RandomHorizontalFlip(),  # 随机旋转
+             transforms.ToTensor(),
+             # 把shape=(H x W x C) 的像素值为 [0, 255] 的 PIL.Image 和 numpy.ndarray转换成shape=(C,H,WW)的像素值范围为[0.0, 1.0]的 torch.FloatTensor
+             transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+             ]
+        ),
+        "test": transforms.Compose(
+            [transforms.Resize((224, 224)),
+             transforms.ToTensor(),
+             transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+             ]
+        )
     }
 
-    data_root = "/home/zheng/PycharmProjects/Computer-vision-proj/data_set/flower_data/"  # 数据集路径
-    image_datasets = {x: datasets.ImageFolder(root=data_root + x,
-                                              transform=data_transform[x])
-                      for x in ["train", "val"]}
-    dataloaders = {x: DataLoader(image_datasets[x], batch_size=16,
-                                 shuffle=True, num_workers=4)
-                   for x in ["train", "val"]}
+    train_dataset = datasets.ImageFolder(root=os.path.join(data_path, "train"), transform=data_transform["train"])
+    vaild_dataset = datasets.ImageFolder(root=os.path.join(data_path, "val"), transform=data_transform["test"])
+    train_num = len(train_dataset)
+    test_num = len(vaild_dataset)
 
-    dataset_sizes = {x: len(image_datasets[x]) for x in ["train", "val"]}
-    class_names = image_datasets["train"].classes
+    n_works = min([os.cpu_count(), batch_size if batch_size > 1 else 0, 9])
+    print('Using {} dataloader workers every process'.format(n_works))
+
+    train_loader = DataLoader(train_dataset, batch_size, shuffle=True, num_workers=n_works, drop_last=True)
+    valid_loader = DataLoader(vaild_dataset, batch_size=batch_size, shuffle=False, num_workers=n_works, drop_last=True)
+
+    print('using {} images for traing and using {} images for testing'.format(train_num, test_num))
+
+    epochs = 40
+    save_path = os.path.join(os.getcwd(), 'checkpoints/alex')
+    if os.path.isdir(save_path):
+        print("checkpoints save in " + save_path)
+    else:
+        os.makedirs(save_path)
+        print("new a dir to save checkpoints: " + save_path)
+
+    best_acc = 0.0
+    train_steps = len(train_loader)
+
+    # training
+    for epoch in range(epochs):
+        time_start = time.time()
+        net.train()
+        running_loss = 0.0
+        for step, data in enumerate(train_loader):
+            images, labels = data
+            optimizer.zero_grad()
+            outputs = net(images.to(device))
+            loss = loss_function(outputs, labels.to(device))
+            loss.backward()
+            optimizer.step()
+
+            # print statistics
+            running_loss += loss.item()
+            print("Epoch" + str(epoch) + ": processing:" + str(step) + "/" + str(train_steps))
+
+        # validate
+        time_end = time.time()
+        net.eval()
+        acc = 0.0  # accumulate accurate number / epoch
+        with torch.no_grad():
+            for val_data in valid_loader:
+                val_images, val_labels = val_data
+                outputs = net(val_images.to(device))
+                predict_y = torch.max(outputs, dim=1)[1]
+                acc += torch.eq(predict_y, val_labels.to(device)).sum().item()
+
+        val_accurate = acc / test_num
+        print('[epoch %d] train_loss: %.3f  val_accuracy: %.3f time_one_epoch: %.3f ' %
+              (epoch + 1, running_loss / train_steps, val_accurate, (-time_start + time_end)))
+
+        if val_accurate > best_acc:
+            best_acc = val_accurate
+            torch.save(net.state_dict(), os.path.join(save_path, 'alex_flower.pth'))
+
+    print('Finished Training')
 
 
-    
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+if __name__ == '__main__':
+    main()
 
-    # 2. 定义网络
-    model = AlexNet(num_classes=5)
-    model.to(device)
-
-    # 3. 定义损失函数和优化器
-    criterion = nn.CrossEntropyLoss()
-    optimizer = optim.Adam(model.parameters(), lr=0.0001)
-
-    # 4. 训练网络
-    epoch = 40
-    
-
-            
 
 
 
